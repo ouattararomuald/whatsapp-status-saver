fa3b85aec989d614375ca2ad629980a8725def6e
Romuald OUATTARA <rouattar@bouyguestelecom.fr>
2020-07-05 23:53:14 +0200

    Save medias

---
+++ app/src/main/java/com/ouattararomuald/statussaver/core/FileHelper.kt
@@ -0,0 +1,61 @@
+package com.ouattararomuald.statussaver.core
+
+import android.os.Environment
+import com.ouattararomuald.statussaver.common.SAVED_MEDIA_DESTINATION_FOLDER_NAME
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import okio.buffer
+import okio.sink
+import okio.source
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import kotlin.coroutines.CoroutineContext
+
+class FileHelper : CoroutineScope {
+  private val job = SupervisorJob()
+
+  override val coroutineContext: CoroutineContext
+    get() = Dispatchers.IO + job
+
+  private val handler = CoroutineExceptionHandler { _, exception ->
+  }
+
+  fun writeFile(outputStream: OutputStream, fileToWrite: File) {
+    launch(handler) {
+      outputStream.use {
+        val sink = it.sink().buffer()
+        sink.writeAll(fileToWrite.source())
+        sink.close()
+      }
+    }
+  }
+
+  fun writeFile(fileToWrite: File, onFinishBlock: suspend CoroutineScope.() -> Unit) {
+    launch(handler) {
+      val fileName = fileToWrite.name
+      val destinationFile = File(
+        Environment.getExternalStorageDirectory(),
+        "$SAVED_MEDIA_DESTINATION_FOLDER_NAME/${fileName}"
+      )
+
+      val fileOutputStream = FileOutputStream(destinationFile)
+      try {
+        fileOutputStream.write(android.R.attr.data)
+        onFinishBlock()
+      } catch (e: IOException) {
+      } finally {
+        fileOutputStream.close()
+      }
+    }
+  }
+
+  fun cancelTasks() {
+    cancel()
+  }
+}
--- app/src/main/java/com/ouattararomuald/statussaver/videos/ui/VideoPlayerActivity.kt
+++ app/src/main/java/com/ouattararomuald/statussaver/videos/ui/VideoPlayerActivity.kt
@@ -1,14 +1,17 @@
 package com.ouattararomuald.statussaver.videos.ui
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import androidx.core.view.isVisible
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource
@@ -16,10 +19,15 @@ import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.material.snackbar.Snackbar
 import com.ouattararomuald.statussaver.Media
 import com.ouattararomuald.statussaver.R
+import com.ouattararomuald.statussaver.common.VIDEO_MIME_TYPE
+import com.ouattararomuald.statussaver.core.FileHelper
 import com.ouattararomuald.statussaver.databinding.ActivityVideoPlayerBinding
-import com.ouattararomuald.statussaver.home.presenters.HomePresenter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -30,6 +38,7 @@ class VideoPlayerActivity : AppCompatActivity() {
   companion object {
     private const val SELECTED_VIDEO_INDEX_KEY = "selected_video_index_key"
     private const val VIDEO_KEY = "video_key"
+    private const val RQ_CREATE_FILE = 0xbee
 
     /**
      * Whether or not the system UI should be auto-hidden after
@@ -55,8 +64,11 @@ class VideoPlayerActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityVideoPlayerBinding
   private lateinit var videos: List<Media>
+  private var videoToWriteIndex = 0
   private var selectedVideoIndex = 0
+  private var isFabExpanded = false
 
+  private var fileHelper = FileHelper()
   private var player: SimpleExoPlayer? = null
 
   private var playWhenReady = true
@@ -79,6 +91,14 @@ class VideoPlayerActivity : AppCompatActivity() {
       selectedVideoIndex = intent.getIntExtra(SELECTED_VIDEO_INDEX_KEY, 0)
     }
 
+    binding.optionsVideoButton.setOnClickListener {
+      if (isFabExpanded) {
+        hideSubMenus()
+      } else {
+        showSubMenus()
+      }
+    }
+
     binding.shareVideoButton.setOnClickListener {
       if (selectedVideoIndex >= 0 && selectedVideoIndex < videos.size) {
         startActivity(Intent.createChooser(
@@ -88,14 +108,91 @@ class VideoPlayerActivity : AppCompatActivity() {
       }
     }
 
+    binding.saveVideoButton.setOnClickListener {
+      val videoMedia = videos[selectedVideoIndex]
+      videoToWriteIndex = selectedVideoIndex
+      saveFile(videoMedia.file)
+    }
+
     supportActionBar?.apply {
       setDisplayHomeAsUpEnabled(true)
       hide()
     }
   }
 
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (requestCode == RQ_CREATE_FILE) {
+      if (resultCode == Activity.RESULT_OK) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          val uri = data?.data
+          if (uri != null) {
+            val stream = contentResolver.openOutputStream(uri)
+            stream?.let {
+              fileHelper.writeFile(it, videos[videoToWriteIndex].file)
+            }
+            displaySuccessMessage()
+          }
+        }
+      }
+    }
+  }
+
+  private fun displaySuccessMessage() {
+    Snackbar.make(
+      binding.root,
+      "The file has been successfully saved.",
+      Snackbar.LENGTH_LONG
+    ).setTextColor(resources.getColor(R.color.snackbar_text_color, theme)).show()
+  }
+
+  private fun displayFailureMessage() {
+    Snackbar.make(
+      binding.root,
+      "Oups! Something went wrong while saving the file.",
+      Snackbar.LENGTH_LONG
+    ).setTextColor(resources.getColor(R.color.snackbar_text_color_failure, theme)).show()
+  }
+
+  private fun saveFile(file: File) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      createFile(file)
+    } else {
+      fileHelper.writeFile(file) {
+        withContext(Dispatchers.Main) {
+          displaySuccessMessage()
+        }
+      }
+    }
+  }
+
+  @RequiresApi(Build.VERSION_CODES.O)
+  private fun createFile(file: File) {
+    val fileName = file.name
+    val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+      addCategory(Intent.CATEGORY_OPENABLE)
+      type = VIDEO_MIME_TYPE
+      putExtra(Intent.EXTRA_TITLE, fileName)
+    }
+    startActivityForResult(intent, RQ_CREATE_FILE)
+  }
+
+  private fun showSubMenus() {
+    binding.shareVideoButton.isVisible = true
+    binding.saveVideoButton.isVisible = true
+    binding.optionsVideoButton.setImageDrawable(resources.getDrawable(R.drawable.ic_clear, theme))
+    isFabExpanded = true
+  }
+
+  private fun hideSubMenus() {
+    binding.shareVideoButton.isVisible = false
+    binding.saveVideoButton.isVisible = false
+    binding.optionsVideoButton.setImageDrawable(resources.getDrawable(R.drawable.ic_add, theme))
+    isFabExpanded = false
+  }
+
   private fun enableFullScreen() {
-    if (Build.VERSION.SDK_INT >= 30) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
       window.setDecorFitsSystemWindows(false)
     } else {
       window.setFlags(
@@ -155,7 +252,7 @@ class VideoPlayerActivity : AppCompatActivity() {
     val shareIntent: Intent = Intent().apply {
       action = Intent.ACTION_SEND
       putExtra(Intent.EXTRA_SUBJECT, resources.getText(R.string.send_title))
-      type = "video/*"
+      type = VIDEO_MIME_TYPE
 
       val authority = "${applicationContext.packageName}.fileprovider"
       val fileUri = FileProvider.getUriForFile(
